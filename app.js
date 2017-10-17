35e13866803a94a95737ba4c48f15a6d146cce07
gabietaylor <gabie.taylor@yahoo.com>
2017-10-17 16:17:59 -0400

    deleted to setup accurately

--- app.js
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const Main = require('./Components/Main')
-
-// Render Main in App
-ReactDOM.render( <
-    Main / > , document.getElementById('app')
-)
